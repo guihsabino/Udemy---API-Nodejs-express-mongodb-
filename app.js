0228c09fbd72c24e1172b7c6b8e777ab352bb8be
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-09 11:46:43 -0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, resp) => {
+    return resp.send({ message: 'Tudo ok com o método GET!' })
+});
+
+app.post('/', (req, resp) => {
+    return resp.send({ message: 'Tudo ok com o método POST!' })
+});
+
 app.listen(3000);
 
 module.exports = app;
