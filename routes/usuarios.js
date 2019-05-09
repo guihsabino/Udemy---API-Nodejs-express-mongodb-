4a60eee0ede7ce90c34ffeb13a9f89da2e4f2c63
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-09 15:21:11 -0300

    commit

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Users = require('../model/usuario');
 
 router.get('/', (req, resp) => {
-    return resp.send({ message: 'Tudo ok com o método GET de usuários!' });
-});
-
-router.post('/', (req, resp) => {
-    return resp.send({ message: 'Tudo ok com o método POST de usuários!' });
+    Users.find({}, (err, dados) => {
+        if (err) return resp.send({ error: 'Erro na consulta de usuários!' })
+        return resp.send(dados);
+    })
 });
 
 router.post('/create', (req, res) => {
-    return resp.send({ message: 'Seu usuário foi criado = ' });
+    const { email, password } = req.body;
+
+    if (!email || !password) return resp.send({ error: 'Dados Incompletos' });
+
+    Users.findOne({ email }, (err, dados) => {
+        if (err) return resp.send({ error: 'Erro ao buscar usuário!' });
+        if (dados) return resp.send({ error: 'Usuário já registrado!' });
+
+        Users.create(req.body, (err, dados) => {
+            if (err) return resp.send({ error: 'Erro ao criar usuário!' });
+            return res.send(dados);
+        })
+    });
+
 });
 
 module.exports = router;
