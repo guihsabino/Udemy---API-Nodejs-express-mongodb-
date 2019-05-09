4576cba9b7f40597a89921c93a7fdbbefb70b23e
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-09 17:25:51 -0300

    commit

---
+++ config/config.js
@@ -0,0 +1,8 @@
+// Se o desenvolvedor não setar essa variavel NODE_ENV, ele assume que é desenvolvimento
+const env = process.env.NODE_ENV || 'dev';
+
+const config = () => {
+    switch (env){
+        
+    }
+}
